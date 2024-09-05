d6cd33cb1c0053a9e837658e104207ea52c8338e
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-09-05 22:24:07 +0100

    Ajouter le fichier .env et configurer les variables d'environnement aussi Ajouter ficher  public pour le CSS et JS

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const indexRouter = require('./routes/index');
+require('dotenv').config();
+const PORT = process.env.PORT;
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const indexRouter = require('./routes/index');
+
 app.use('/', indexRouter);
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
 });
