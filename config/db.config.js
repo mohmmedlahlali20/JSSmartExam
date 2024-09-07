b78e28b9cf9ef891b602ec34a07e5401d69ab08c
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-09-07 20:35:04 +0100

    create home page and style it

--- config/db.config.js
+++ config/db.config.js
@@ -6,6 +6,15 @@ class Database {
         this.connection = mysql.createConnection(config);
     }
 
+    /**
+     * Connect to the database.
+     * 
+     * This function connects to the database server specified in the
+     * constructor. If the connection is successful, it logs a success message
+     * to the console. If the connection fails, it logs an error message to the
+     * console.
+     */
+
     connect() {
         this.connection.connect((err) => {
             if (err) {
@@ -16,6 +25,18 @@ class Database {
         });
     }
 
+    /**
+     * Execute a SQL query on the database.
+     * 
+     * This function takes a SQL query string and an optional array of
+     * arguments to be passed to the query. It returns a Promise that resolves
+     * with the query results, or rejects with an error if the query fails.
+     * 
+     * @param {string} sql - A SQL query string.
+     * @param {array} args - An array of arguments to pass to the query.
+     * @returns {Promise} A Promise that resolves with the query results, or
+     * rejects with an error if the query fails.
+     */
     query(sql, args) {
         return new Promise((resolve, reject) => {
             this.connection.query(sql, args, (err, results) => {
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const indexController = require('../controllers/indexController');
 const authController = require('../controllers/authController/register')
+const homeController = require('../controllers/homeController');
+
+router.get('/', homeController.home);
 router.get('/statique', indexController.statique);
 router.get('/register', authController.register);
 module.exports = router;
