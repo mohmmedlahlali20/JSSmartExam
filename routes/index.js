8a17a08233bf9f33993af266459db53e8f9834ff
imane souaouti <souaouti.iman@gmail.com>
2024-09-07 20:27:08 +0100

    Add The login and Register UI. make two pages register and login with html and css

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const indexController = require('../controllers/indexController');
-const authController = require('../controllers/authController/register')
-router.get('/statique', indexController.statique);
-router.get('/register', authController.register);
+const indexController = require("../controllers/indexController");
+const authController = require("../controllers/authController/register");
+const loginController = require("../controllers/authController/login");
+router.get("/statique", indexController.statique);
+router.get("/register", authController.register);
+router.get("/login", loginController.login);
 module.exports = router;
