3921e84767e9166a34cf0cb5376d3b704268cf07
imane souaouti <souaouti.iman@gmail.com>
2024-09-13 10:30:24 +0100

    Add Functionality of Niveaux Crud and also Display the list of test side front end and backend

--- config/db.config.mjs
+++ config/db.config.mjs
@@ -1,22 +1,22 @@
-import mysql from 'mysql2/promise';
+import mysql from "mysql2";
 
 const db = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'mohammed',
-    password: process.env.DB_PASSWORD || 'password',
-    database: process.env.DB_NAME || 'JSSmartExam',
-    connectionLimit: 10, 
-    waitForConnections: true,
-    queueLimit: 0 
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "jssmartexam",
+  connectionLimit: 10,
+  waitForConnections: true,
+  queueLimit: 0,
 });
 
 const checkConnection = async () => {
-    try {
-        await db.query('SELECT 1');
-        console.log('Database connected successfully.');
-    } catch (err) {
-        console.error('Error connecting to the database:', err);
-    }
+  try {
+    await db.query("SELECT 1");
+    console.log("Database connected successfully.");
+  } catch (err) {
+    console.error("Error connecting to the database:", err);
+  }
 };
 
 checkConnection();
--- routes/index.mjs
+++ routes/index.mjs
@@ -1,34 +1,63 @@
-import express from 'express';
-import { home } from '../controllers/homeController.mjs';
-import { statique } from '../controllers/indexController.mjs';
-import { login, logout } from '../controllers/authController/login.mjs';
-import { getRegisterPage, register } from '../controllers/authController/register.mjs';
-import studentsController from '../controllers/formateur/students.mjs';
-import { showClasse, createClass } from '../controllers/classes/classController.mjs';
-import {isAuthenticated} from '../middleware/authMiddleware.mjs'
+import express from "express";
+import { home } from "../controllers/homeController.mjs";
+import { statique } from "../controllers/indexController.mjs";
+import { login, logout } from "../controllers/authController/login.mjs";
+import {
+  getRegisterPage,
+  register,
+} from "../controllers/authController/register.mjs";
+import studentsController from "../controllers/formateur/students.mjs";
+import {
+  showClasse,
+  createClass,
+} from "../controllers/classes/classController.mjs";
+import { isAuthenticated } from "../middleware/authMiddleware.mjs";
+import {
+  createNiveauController,
+  getNiveauxController,
+  updateNiveauController,
+  deleteNiveauController,
+} from "../controllers/niveaux/niveauController.mjs";
+import { getTests } from "../controllers/tests/testController.mjs";
 const router = express.Router();
 const app = express();
 
+router.get("/", home);
+router.get("/statique", isAuthenticated, statique);
 
+router.get("/register", getRegisterPage);
+router.post("/register", register);
+router.get("/login", (req, res) =>
+  res.render("auth/login", { title: "Login Page" })
+);
+router.post("/login", login);
+router.get("/logout", logout);
 
+router.get("/Add-classe", isAuthenticated, showClasse);
+router.post("/create-class", isAuthenticated, createClass);
+router
+  .route("/add_etudiant")
+  .get(studentsController.etudaints)
+  .post(studentsController.addEtudiants);
+router.post(
+  "/add_etudiant/:id",
+  isAuthenticated,
+  studentsController.updateStudent
+);
+router.delete(
+  "/delete_student/:id",
+  isAuthenticated,
+  studentsController.deleteStudent
+);
 
+//niveaux
+router.get("/niveaux", getNiveauxController);
+router.post("/create-niveau", createNiveauController);
+router.post("/update-niveau", updateNiveauController);
+router.post("/delete-niveau/:id", deleteNiveauController);
 
-router.get('/', home);
-router.get('/statique', isAuthenticated, statique);
-
-router.get('/register', getRegisterPage);
-router.post('/register', register);
-router.get('/login', (req, res) => res.render('auth/login', { title: 'Login Page' }));
-router.post('/login', login);
-router.get('/logout', logout);
-
-router.get('/Add-classe', isAuthenticated, showClasse);
-router.post('/create-class', isAuthenticated, createClass);
-router.route('/add_etudiant')
-    .get(studentsController.etudaints)  
-    .post(studentsController.addEtudiants)
-    router.post('/add_etudiant/:id', isAuthenticated, studentsController.updateStudent);
-    router.delete('/delete_student/:id',isAuthenticated, studentsController.deleteStudent);
-app.use('/', router); 
+// tests
+router.get("/tests", getTests);
+app.use("/", router);
 
 export default app;
